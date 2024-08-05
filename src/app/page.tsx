13e239b8257c43e4dffbbfc39afdec4874a3b3a3
MaryamAkberAli <ccamarch202207@gmail.com>
2024-08-05 21:54:01 +0500

    changing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
-export default function Home() {
-  return 
-   <h1>Hello, I am Maryam And this is my first Next.js project</h1>
-  
+export default function homePage(){
+  return <h1>Hello, I am Maryam And this is my first Next.js project </h1>
 }
+  
+
